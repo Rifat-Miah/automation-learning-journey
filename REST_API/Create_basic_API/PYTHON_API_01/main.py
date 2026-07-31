a96438ef239202d021fdda13cf6c4a2c95da0b9c
Rifat Miah <rifatmiah2041@gmail.com>
2026-07-31 23:31:50 +0600

    Created database part

--- REST_API/Create_basic_API/PYTHON_API_01/main.py
+++ REST_API/Create_basic_API/PYTHON_API_01/main.py
@@ -3,6 +3,26 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # Create Database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///travel.db"
+
+db = SQLAlchemy(app)
+
+class Destination(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    destination = db.Column(db.String(50), nullable = False)
+    country = db.Column(db.String(50), nullable = False)
+    rating = db.Column(db.Float, nullable = False)
+
+    def to_dict(self):
+        return{
+            "id": self.id,
+            "destination": self.destination,
+            "country": self.country,
+            "rating": self.rating
+        }
+
+with app.app_context():    # create a db context manager
+    db.create_all()
 
 #Create Routes
 @app.route("/")      # Home page route
